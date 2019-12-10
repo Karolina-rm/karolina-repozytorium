738f63883b44c6976f05310e863c04884bff74d0
Karolina-rm <karolina.ra.kr@gmail.com>
2019-12-10 09:30:14 +0100

    change

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args){
+
+        System.out.println("First unit test:");
+
+        Calculator myFirstCalculator = new Calculator();
+
+        int subtraction = myFirstCalculator.subtract(5, 2);
+        if (subtraction == 3) {
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Test failed");
+        }
+
+        System.out.println("Second unit test:");
+
+        Calculator mySecondCalculator = new Calculator();
+
+        int addition = mySecondCalculator.add(1,3);
+        if (addition == 4) {
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Test failed");
+        }
+
+
+
     }
 }
