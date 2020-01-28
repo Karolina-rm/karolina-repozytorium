0bf068d9008c359ae7185642ac7ebf97b1779fd4
Maciej Rudnicki <maciejru@amazon.com>
2020-01-28 17:19:09 +0100

    Clean up

--- puzzles/src/main/java/com/kodilla/Button.java
+++ puzzles/src/main/java/com/kodilla/Button.java
@@ -1,26 +1,43 @@
 package com.kodilla;
 
-import javafx.event.EventHandler;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.text.Font;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+import javafx.event.EventHandler;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.text.Font;
+
 public class Button extends javafx.scene.control.Button {
 
+    private final Image imageBeforeClick = new Image(getClass().getResource("/blue_button00.png").toExternalForm());
+    private final Image imageAfterClick = new Image(getClass().getResource("/blue_button02.png").toExternalForm());
     private final String FONT_PATH = "kenvector_future_thin.ttf";
     private final String PRESSED_BUTTON = "-fx-background-color: transparent; -fx-background-image: url('blue_button02.png');";
     private final String UNPRESSED_BUTTON = "-fx-background-color: transparent; -fx-background-image: url('blue_button00.png');";
 
     public Button(String text) {
+
+        BackgroundImage backgroundImage = new BackgroundImage(
+                imageBeforeClick,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+        Background background = new Background(backgroundImage);
+
         setText(text);
         setFont();
         setPrefWidth(190);
         setPrefHeight(49);
-        setStyle(UNPRESSED_BUTTON);
+        setBackground(background);
         setButtonOptions();
     }
 
@@ -36,13 +53,22 @@ public class Button extends javafx.scene.control.Button {
     private void setUnpressedButtonStyle() {
         setStyle(UNPRESSED_BUTTON);
         setPrefHeight(40);
-        setLayoutY(getLayoutY() -4);
+        setLayoutY(getLayoutY() - 4);
     }
 
     private void setPressedButtonStyle() {
-        setStyle(PRESSED_BUTTON);
-        setPrefHeight(35);
-        setLayoutY(getLayoutY() +4);
+
+        BackgroundImage backgroundImage = new BackgroundImage(
+                imageAfterClick,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+        Background background = new Background(backgroundImage);
+
+        setBackground(background);
+        setPrefHeight(80);
+        setLayoutY(getLayoutY() + 4);
 
     }
 
@@ -70,7 +96,7 @@ public class Button extends javafx.scene.control.Button {
             @Override
             public void handle(MouseEvent event) {
                 setEffect(new DropShadow());
-                }
+            }
         });
 
         setOnMouseExited(new EventHandler<MouseEvent>() {
@@ -79,8 +105,6 @@ public class Button extends javafx.scene.control.Button {
                 setEffect(null);
             }
         });
-
-
 
     }
 }
